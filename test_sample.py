a2647e0b1a3ced89976bb5478649ed436710756d
Evgenii <34813529+EvgeniiMoskovtsev@users.noreply.github.com>
2024-08-06 10:44:38 +0300

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -1,6 +1,16 @@
-def inc(x):
-    return x + 1
+import torch
 
+cuda_available = torch.cuda.is_available()
+print(f"CUDA доступна: {cuda_available}")
 
-def test_answer():
-    assert inc(3) == 4
+if cuda_available:
+    gpu_name = torch.cuda.get_device_name(0)
+    cuda_version = torch.version.cuda
+    print(f"Используемый GPU: {gpu_name}")
+    print(f"Версия CUDA: {cuda_version}")
+    a = torch.tensor([1.0, 2.0, 3.0], device='cuda')
+    b = torch.tensor([4.0, 5.0, 6.0], device='cuda')
+    c = a + b
+    print(f"Результат a + b на GPU: {c}")
+else:
+    print("CUDA не доступна")
