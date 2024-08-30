cc35b2734c916ae4b940df242deb8bf973bef78a
Evgenii <34813529+EvgeniiMoskovtsev@users.noreply.github.com>
2024-08-30 10:32:28 +0300

    Create test_mnist_local_dataset.py

---
+++ test_mnist_local_dataset.py
@@ -0,0 +1,117 @@
+from clearml import Dataset
+import os
+import urllib.request
+import tarfile
+import torch
+from torch import nn, optim
+import torch.nn.functional as F
+import torchvision
+from torchvision import datasets, transforms
+
+EPOCHS = 5
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 20, 5, 1)
+        self.conv2 = nn.Conv2d(20, 50, 5, 1)
+        self.fc1 = nn.Linear(4*4*50, 500)
+        self.fc2 = nn.Linear(500, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(-1, 4*4*50)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+def train(model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 10 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+def test(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+def download_mnist():
+    dataset_name = "MNISTDataset_2"
+    dataset_project = "TestProject"
+
+    dataset_path = Dataset.get(
+        dataset_name=dataset_name, 
+        dataset_project=dataset_project,
+        alias="MNIST dataset"
+    ).get_local_copy()
+    
+    dataset_name = "MNISTDataset_2"
+    dataset_project = "TestProject"
+    
+    dataset_path += "/MNIST_2"
+    return dataset_path
+    
+
+
+def main():
+    print("PyTorch version:", torch.__version__)
+    print("Torchvision version:", torchvision.__version__)
+
+    cuda_available = torch.cuda.is_available()
+    print(f"CUDA доступна: {cuda_available}")
+    
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('Using device:', device)
+
+    dataset_path = download_mnist()
+    print("Dataset MNIST loaded successfully.")
+
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST(dataset_path, train=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=128, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST(dataset_path, train=False, transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=128, shuffle=True)
+
+
+    model = Net().to(device)
+    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+
+    for epoch in range(1, EPOCHS + 1):
+        train(model, device, train_loader, optimizer, epoch)
+        test(model, device, test_loader)
+
+if __name__ == "__main__":
+    main()
